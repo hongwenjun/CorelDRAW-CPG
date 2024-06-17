51f19c6067c5f499866f67e1a4a9f09c153f38bf
蘭雅sRGB <hongwenjun@gmail.com>
2024-06-17 22:48:46 +0800

    02_WelcomeScreen 和 03_ClearFill 功能: 点击图标开启工具箱窗口

---
+++ 04_ToolsBox/resource.h
@@ -0,0 +1,7 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_WELCOME                             9
+#define IDC_NEWDOC                              1001
+#define IDC_LASTDOC                             1002
