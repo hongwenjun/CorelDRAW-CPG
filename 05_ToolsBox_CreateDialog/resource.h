bf160b98fa565d38ca0296f091d8a0595ecfb4cd
蘭雅sRGB <hongwenjun@gmail.com>
2024-06-19 23:27:43 +0800

    创建非模态对话框，使用附加数据中传递 m_pApp 指针

---
+++ 05_ToolsBox_CreateDialog/resource.h
@@ -0,0 +1,9 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_TOOLS_BOX                           100
+#define IDC_NEWDOC                              1001
+#define IDC_LASTDOC                             1002
+#define IDC_CLEAR_FILL                          1003
+#define IDC_SR_FLIP                             1004
